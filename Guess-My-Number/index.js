12da0598cddba9c4f910b5cc9f62f51e73bf5770
vinayande01 <166700212+vinayande01@users.noreply.github.com>
2024-04-13 07:02:56 +0000

    Updated

--- Guess-My-Number/index.js
+++ Guess-My-Number/index.js
@@ -47,3 +47,41 @@ document.querySelector(".again").addEventListener("click", function(){
     document.querySelector('.number').style.width="15rem";
     document.querySelector('body').style.backgroundColor=" #222"
 })
+// Enter button configrartion
+
+document.addEventListener('downkey',function(e){
+    if (e.key === "Enter") {
+        var Guessnumber=Number(document.querySelector('.guess').value)
+// Condections...
+    if (Guessnumber !== 0){
+        if (Score>1){
+            if (Guessnumber === SecretNumber){
+                document.querySelector('.message').textContent="🎉 Great You Won The Game";
+                document.querySelector(".highscore").textContent=Score;
+                document.querySelector(".number").textContent=SecretNumber;
+                Highscore= Highscore < Score ? Score : Highscore;
+                document.querySelector('.highscore').textContent=Highscore
+                document.querySelector('body').style.backgroundColor="Purple"
+                document.querySelector('.number').style.width="30rem"
+            }
+            else if (Guessnumber !== SecretNumber){
+                var res=Guessnumber > SecretNumber ? "📈To High" : "📉To Low";
+                Score=Score-1
+                document.querySelector(".score").textContent=Score
+                document.querySelector(".message").textContent=res
+                
+            }
+        }
+        else{
+            document.querySelector(".score").textContent=0
+            document.querySelector(".message").textContent="You Fucker Lost The Game😂"
+        }
+
+    }
+    else {
+        document.querySelector(".message").textContent="Look☝️Top-Right-Cornor You Fucker👉"
+    }
+
+    }
+
+})
